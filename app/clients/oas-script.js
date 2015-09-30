714f36ffeb6fef67f8c866b0913ef502f8773fb4
Horace <Horace@macbook.local>
2015-09-30 00:55:04 -0400

    Fixed OAS script client.

--- app/clients/oas-script.js
+++ app/clients/oas-script.js
@@ -1,16 +1,16 @@
 (function(__ad_data) {
-	if (!__ad_data || !__ad_data.tag || __ad_data.tag === null) return;
+    if (!__ad_data || !__ad_data.tag || __ad_data.tag === null) return;
+    var tag = __ad_data.tag;
+    OAS_url = tag.url;
+    OAS_sitepage = tag.sitepage;
 
-	OAS_url = __ad_data.url;
-	OAS_sitepage = __ad_data.sitepage;
+    OAS_pos = tag.pos || "";
+    OAS_query = tag.query || "";
+    OAS_exclude = tag.exclude || "";
 
-	OAS_pos = __ad_data.pos || "";
-	OAS_query = __ad_data.query || "";
-	OAS_exclude = __ad_data.exclude || "";
+    var OAS_RN = new String(Math.random());
+    var OAS_RNS = OAS_RN.substring(2, 11);
 
-	var OAS_RN = new String(Math.random());
-	var OAS_RNS = OAS_RN.substring(2, 11);
-
-	document.write('<scr' + 'ipt language="javascript" type="text/javascript" src="' + OAS_url + '/RealMedia/ads/adstream_jx.ads/' + OAS_sitepage + '/1' + OAS_RNS + '@' + OAS_pos + '?_RM_Exclude_=' + OAS_exclude + '&' + OAS_query + '"></scr' + 'ipt>');
+    document.write('<scr' + 'ipt language="javascript" type="text/javascript" src="' + OAS_url + '/RealMedia/ads/adstream_jx.ads/' + OAS_sitepage + '/1' + OAS_RNS + '@' + OAS_pos + '?_RM_Exclude_=' + OAS_exclude + '&' + OAS_query + '"></scr' + 'ipt>');
 
 })(__ad_data);
